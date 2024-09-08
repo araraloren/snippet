a69f6b435bc8727ad4ba57448d4720ff01d11238
araraloren <blackcatoverwall@gmail.com>
2024-09-08 17:41:13 +0800

    don't update wit if not local

--- packages/snippet/build.rs
+++ packages/snippet/build.rs
@@ -1,5 +1,9 @@
 fn main() -> color_eyre::Result<()> {
-    wit_deps::lock_sync!()
-        .map_err(|e| color_eyre::eyre::eyre!("can not sync dependencies of snippet-c: {e:?}"))?;
+    #[cfg(feature = "local")]
+    {
+        wit_deps::lock_sync!().map_err(|e| {
+            color_eyre::eyre::eyre!("can not sync dependencies of snippet-c: {e:?}")
+        })?;
+    }
     Ok(())
 }
